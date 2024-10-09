f7ab6fcb7acdc586277efba958f3b853977059a3
Tulio Cruz <fonsecacruztulio@gmail.com>
2024-10-09 20:33:31 -0300

    Laravel + Breeze

---
+++ app/Models/auth/login.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\auth;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class login extends Model
+{
+    use HasFactory;
+}
